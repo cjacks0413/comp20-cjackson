37f6865b0071c68c9f1dd67d51e8cc122867d3b6
cjacks0413 <cameron.jackson@tufts.edu>
2013-02-12 22:44:38 -0500

    added compatibility check

--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -4,9 +4,13 @@ mspacman.src = "pacman10-hp-sprite.png";
 function render_game()
 {
 	canvas = document.getElementById('mspacman'); 
-	ctx = canvas.getContext('2d');
-	
-	ctx.drawImage(mspacman, 320, 0, 465, 135, 0, 0, 500, 300); 
-	ctx.drawImage(mspacman, 120, 0, 50, 20, 300, 10, 40, 40); 
-	ctx.drawImage(mspacman, 40, 160, 20, 20, 50, 210, 30, 30); 
+	if (canvas.getContext) {
+		ctx = canvas.getContext('2d');
+		ctx.drawImage(mspacman, 320, 0, 465, 135, 0, 0, 500, 300); 
+		ctx.drawImage(mspacman, 120, 0, 50, 20, 300, 10, 40, 40); 
+		ctx.drawImage(mspacman, 40, 160, 20, 20, 50, 210, 30, 30); 
+	}
+	else {
+		alert('Sorry, canvas is not supported in your browser!'); 
+	}
 }
