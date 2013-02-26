c9c52b5ac54d62f771f6265e8eea3fc026e0626b
cjacks0413 <cameron.jackson@tufts.edu>
2013-02-26 11:55:42 -0500

    identify current location

--- where/map.js
+++ where/map.js
@@ -1,20 +1,67 @@
-var myLat = 0;
-var myLng = 0;
-var request = new XMLHttpRequest();
-var iamHere = new google.maps.LatLng(myLat, myLng);
-var mapOptions = {
-		zoom = 7;
-		center = me;
-		mapTypeId: google.maps.MapTypeId.ROADMAP;
+myLat = 0;
+myLng = 0;
+request = new XMLHttpRequest();
+iamHere = new google.maps.LatLng(myLat, myLng);
+mapOptions = {
+		zoom:7,
+		center:me,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
-var map;
-var marker;
-var infowindow = new google.maps.InfoWindow();
-var places; 
+map;
+marker;
+infowindow = new google.maps.InfoWindow();
+places; 
 
 function init()
 {
 	map = new google.maps.Map(document.getElementById("map"), mapOptions); 
-	
+	findMyPosition();
+	alert("Called Init");
 }
 
+function findMyPosition()
+{
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+		myLat = position.coords.latitude;
+		myLng = position.coords.longitude;
+		renderMap();
+		}); 
+	}
+	else {
+		alert("HTML5 geolocation not supported by your browser.");
+	}		
+}
+
+function renderMap()
+{
+	me = new google.maps.LatLng(myLat, myLng);
+	map.Panto(me);
+	
+	//edit marker
+	marker = new google.maps.Marker({
+		position: me,
+		title:"I Am Here",
+		})
+	marker.setMap(map);
+	
+	//info markers for redLine station?
+	var request = {
+		location: me, 
+		radius: '500',
+		types: [????],
+	};
+	//? 
+	//service = new google.maps.places.PlacesService(map);
+	//service.search(request, callback);
+	//within render line, call the google maps API and make the array with the JSON 
+	//AJAX link. hardcode the latitudes of the info box, AJAX the info inside them. 
+//	renderRedLine();
+}
+
+function renderRedLine() 
+{
+	Alewife = new google.maps.LatLng()
+	
+	Alewife = new marker
+}
